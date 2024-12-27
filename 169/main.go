2ffc7cd841bbb0295cd4ff50317063bb28f8b5cb
ducnt114 <trungduc110492@gmail.com>
2024-12-27 23:58:10 +0700

    update

---
+++ 169/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(majorityElement([]int{3, 2, 3}))
+	fmt.Println(majorityElement([]int{2, 2, 1, 1, 1, 2, 2}))
+}
+
+func majorityElement(nums []int) int {
+	count := 0
+	candidate := 0
+
+	for _, num := range nums {
+		if count == 0 {
+			candidate = num
+		}
+		if num == candidate {
+			count++
+		} else {
+			count--
+		}
+	}
+
+	return candidate
+}
